ff70a474d86dd376224649c2d210192ce726ea2c
RospikaSiregar <rospikasarahsiregar@gmail.com>
2024-11-06 17:15:25 +0700

    PAIR 16

--- T04.java
+++ T04.java
@@ -5,7 +5,7 @@
 import java.util.*;
 import java.lang.Math;
 
-public class Program {
+public class T04 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
